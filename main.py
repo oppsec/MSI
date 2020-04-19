72654f1acd935ed5a92dbc70e528a27fbfe79349
Daniel Moura <danielsdm.contato.@gmail.com>
2020-04-19 15:55:04 -0300

    file

---
+++ main.py
@@ -0,0 +1,17 @@
+import system_info
+import os
+
+W  = '\033[0m'  # normal
+
+def choose_option():
+
+    print(W+'')
+    print('[1] - System Info ')
+    option = int(input('Select? '))
+
+    if(option == 1):
+       system_info.system1()
+
+if(__name__ == "__main__"):
+    choose_option()
+
---
+++ system_info.py
@@ -0,0 +1,37 @@
+import psutil, datetime
+import sys
+import main
+import os
+
+W  = '\033[0m'  # normal
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+
+def system1():
+
+    time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    mem = psutil.virtual_memory()
+    per = psutil.cpu_percent(interval=1)
+    count = psutil.cpu_count()
+    freq = psutil.cpu_freq()
+    stats = psutil.cpu_stats()
+
+    os.system("cls") 
+    print(W+'---my-sys-info---')
+    print(W+'*******************')
+    print(W+'---CPU---')
+    print(O+'CPU %:', per)
+    print(O+'CPU Frequence:', freq.current)
+    print(O+'CPU Cores:', count)
+    print(O+'CPU Stats:', stats.ctx_switches)
+    print(W+'')
+    print(W+'---MEMORY---')
+    print(O+'Memory %:', mem.percent)
+    print(O+'Memory Available:', mem.available)
+    print(O+'Memory Used:', mem.used)
+    print(W+'')
+    print(W+'---BOOT---')
+    print(O+'Boot Time:', time)
+
+if(__name__ == "__main__"):
+    system1()
