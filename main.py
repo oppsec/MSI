e127790254e9d11451fdd1ed25aa4622248ae3b7
dsm <50470310+dsmuix@users.noreply.github.com>
2020-06-22 09:28:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import os
 
 def main():
    try:
-      print('[1] - System Info ')
-      option = int(input('Select? '))
+      print('*****************')
+      print('[1] - System Info')
+      option = int(input('[?] - Select: '))
 
       if option == 1:
          system1()
       else:
-          print("Select a valid option")
+          print("[x] - Select a valid option")
    except KeyboardInterrupt:
-      print("You close the program. :(")
+      print("[!] - You closed the program.")
 
 if __name__ == "__main__":
     main()
