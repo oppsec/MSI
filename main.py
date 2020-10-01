6db01d819c7f8ce71b414794d4df740bfbc52a80
yatoxpl <yatoxpl@yahoo.com>
2020-09-30 23:27:35 -0400

    Modify some things

--- main.py
+++ main.py
@@ -1,52 +1,45 @@
-import socket
-import os
+#!/usr/bin/env python3
 
-from datetime import datetime # Time
-from rich import print # CLI Color
-from psutil import virtual_memory, cpu_count, cpu_freq, cpu_percent, boot_time # Get system info
+from socket import gethostname
+from os import system
+from os import name as os_name
 
-ascii = """[bold magenta]
+from datetime import datetime
+from rich import print
+from psutil import virtual_memory, cpu_count, cpu_freq, cpu_percent, boot_time
+
+ascii = '''[bold magenta]
                         _ 
         ____ ___  _____(_)
        / __ `__ \/ ___/ / 
       / / / / / (__  ) /  
-     /_/ /_/ /_/____/_/ 
+     /_/ /_/ /_/____/_/   
 
- my sistem info 2.1 | oppsec[/bold magenta]      
-"""
-
-dots = "[bold white]. . . . . . . . . . . . . . . .[/bold white] \n"
+- My System Info 2.1 | @oppsec[/bold magenta]'''
 
 def clearTerminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
+  system('cls' if os_name == 'nt' else 'clear')
 
 def menu():
-    clearTerminal()
-
-    ## BANNER
-    print(ascii)
-    print(dots)
-
-    username = socket.gethostname()
-    print(f"[!] - Welcome [bold magenta]{username}[/bold magenta]", ":smiley:", "\n")
-
-    ## CALL SYSTEM INFO FUNCTION
-    sysinfo()
+  bar = (f'[bold white]. . . . . . . . . . . . . . . .[/bold white]\n')
+  username = gethostname()
+  print(ascii)
+  print(f'[!] - Welcome [bold magenta]{username}[/bold magenta] :smiley:\n')
+  print(bar)
+  sysinfo()
+  print(bar)
 
 def sysinfo():
+  availableMem = round(virtual_memory().total / (1024.0 ** 3), 2)
+  cpuFreq = cpu_freq().current
+  cpuCount = cpu_count()
+  cpuPercent = cpu_percent()
+  bootTime = datetime.fromtimestamp(boot_time()).strftime('%Y-%m-%d %H:%M:%S')
 
-    availableMemory = virtual_memory()
-    cpuFreq = cpu_freq()
-    bootTime = datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-
-    print('[bold green]#[/bold green] Available memory:', availableMemory.available, '\n')
-
-    print('[bold green]#[/bold green] CPU Amount:', cpu_count())
-    print('[bold green]#[/bold green] CPU Frequency:', cpuFreq.current)
-    print('[bold green]#[/bold green] CPU Use Percentage:', cpu_percent(), '\n')
-
-    print('[bold green]#[/bold green] Boot Time:', bootTime, '\n')
-
-    print(dots)
+  print(f'[bold green]#[/bold green] Available memory: {availableMem} GB\n')
+  print(f'[bold green]#[/bold green] CPU Amount: {cpuCount}')
+  print(f'[bold green]#[/bold green] CPU Frequency: {cpuFreq}')
+  print(f'[bold green]#[/bold green] CPU Usage Percentage: {cpuPercent}\n')
+  print(f'[bold green]#[/bold green] Boot Time: {bootTime}\n')
 
 menu()
